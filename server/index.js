6fd12a61b445bd5792ef163149a0ef151bcc0979
Datnt <datnt.pq23@gmail.com>
2023-03-01 23:19:07 +0700

    commit layout login and register

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const color = require("colors");
 
 const app = express();
 require("dotenv").config();
@@ -8,18 +9,28 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+// Connect Database MongoDB
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("DB Connection Successful!");
+    console.log(
+      `DB Connection Successful: ${mongoose.connection.host}`.cyan.underline
+    );
   })
   .catch((err) => {
-    console.log(err.message);
+    console.log(`Error: ${err.message}`.red.bold);
   });
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server Started on Port ${process.env.PORT}`);
-});
+const Port = process.env.PORT;
+app.listen(Port, console.log(`Server Started on Port ${Port}`.yellow.bold));
