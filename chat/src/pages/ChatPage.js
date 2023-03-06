1f3e608d035cb0607b0be0c74c21724d56bc58d2
Datnt <datnt.pq23@gmail.com>
2023-03-07 01:03:54 +0700

    Update code create Group Chats

--- chat/src/pages/ChatPage.js
+++ chat/src/pages/ChatPage.js
@@ -1,22 +1,28 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { Box } from "@chakra-ui/react";
+import { ChatState } from "../contexts/ChatProvider";
+import SideDrawer from "../components/structure/SideDrawer";
+import ChatBox from "../components/ChatBox";
+import MyChat from "../components/MyChats";
+import { useState } from "react";
 
 const ChatPage = () => {
-  const [chats, setChats] = useState([]);
-  const fetchChats = async () => {
-    const data = await axios.get("/api/chat");
-    setChats(data);
-  };
-
-  useEffect(() => {
-    fetchChats();
-  }, []);
-
+  const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
   return (
-    <div>
-      {/* {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-      ))} */}
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display={"flex"}
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChat fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
     </div>
   );
 };
